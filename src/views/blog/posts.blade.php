44bc684a285f7ba414cde1807c546fe20616bd05
lembarek <lembarek2011@gmail.com>
2016-06-07 22:26:44 +0100

    add title to url for seo purpose

--- src/views/blog/posts.blade.php
+++ src/views/blog/posts.blade.php
@@ -8,7 +8,7 @@
     <div class="posts">
         @foreach($posts as $post)
             <div class="post">
-                <a href="{{ route('blog::show_post', [$post->slug]) }}">{{ $post['title'] }}</a>
+                <a href="{{ route('blog::show_post', [$post->slug]) }}" title="{{ $post->title }}">{{ $post['title'] }}</a>
                 <p>{{ substr($post['body'], 0, 80) }}...</p>
             </div>
         @endforeach
