2d4e02eb44aa5495581cdceacb5a49030161413f
lembarek <lembarek2011@gmail.com>
2016-06-26 21:51:17 +0100

    add some field to tags table

--- src/migrations/2016_06_25_200157_create_tags_table.php
+++ src/migrations/2016_06_25_200157_create_tags_table.php
@@ -14,7 +14,13 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('page_image');
+            $table->string('meta_description');
+            $table->string('layout')->default('blog::tag.index');
+            $table->boolean('reverse_direction');
             $table->timestamps();
         });
     }
