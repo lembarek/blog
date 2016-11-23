77164dd55aa74b8115150eb17d6511edc33d9038
lembarek <lembarek2011@gmail.com>
2016-11-23 06:24:44 +0100

    remove the defaults values from config/blog.php

--- src/config/blog.php
+++ src/config/blog.php
@@ -4,11 +4,11 @@ return [
     'title' => 'title of the blog',
     'description' => 'description of the blog',
     'lang' => 'en',
-    'twitter_name' => '@lembarekbenghez',
-    'facebook_name' => 'lembarek2011',
-    'linkedin_name' => 'lembarek2011',
-    'githup_name' => 'lembarek2011',
-    'google_plus_name' => '+lembarek2011',
+    'twitter_name' => '',
+    'facebook_name' => '',
+    'linkedin_name' => '',
+    'github_name' => '',
+    'google_plus_name' => '',
 
     'rss_size' => 20,
 ];
