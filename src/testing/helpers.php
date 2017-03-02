768f4e0fb8002f90162df979ddf7f377ef0d6da1
lembarek <lembarek2011@gmail.com>
2017-03-02 16:59:26 +0100

    use ufactory in place of factory

--- src/testing/helpers.php
+++ src/testing/helpers.php
@@ -6,32 +6,32 @@ use Lembarek\Blog\Models\Category;
 
 function createPost($overs = [], $limit=1)
 {
-    return factory(Post::class, $limit)->create($overs);
+    return ufactory(Post::class, $limit)->create($overs);
 }
 
 function makePost($overs = [], $limit=1)
 {
-    return factory(Post::class, $limit)->make($overs);
+    return ufactory(Post::class, $limit)->make($overs);
 }
 
 function createTag($overs = [], $limit=1)
 {
-    return factory(Tag::class, $limit)->create($overs);
+    return ufactory(Tag::class, $limit)->create($overs);
 }
 
 function makeTag($overs = [], $limit=1)
 {
-    return factory(Tag::class, $limit)->make($overs);
+    return ufactory(Tag::class, $limit)->make($overs);
 }
 
 function createCategory($overs = [], $limit=1)
 {
-    return factory(Category::class, $limit)->create($overs);
+    return ufactory(Category::class, $limit)->create($overs);
 }
 
 function makeCategory($overs = [], $limit=1)
 {
-    return factory(Category::class, $limit)->make($overs);
+    return ufactory(Category::class, $limit)->make($overs);
 }
 
 
